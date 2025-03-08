bb60b83d395fe8090c1129f29d385977cf35a8bf
NagatsumaHiroki <act1911@gmail.com>
2025-03-08 20:53:02 +0900

    docker導入対応
    docker-compose.yaml作成
    Dockerfile作成
    README.md
    vite.config.tsの変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import env from "vite-plugin-env-compatible"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
