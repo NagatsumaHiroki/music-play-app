d0dc88abde794ef163709c750b18d2f912abc081
NagatsumaHiroki <act1911@gmail.com>
2025-03-08 21:02:23 +0900

    トークン取得APIエラー対応
    searchSongInfoの対応漏れ修正

--- src/api/searchSongInfo.ts
+++ src/api/searchSongInfo.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import token from '../lib/fetchToken';
+import fetchToken from '../lib/fetchToken';
 
   interface  SearchProps {
     keyword: string;
   };
 
   export async function searchSongInfo(props: SearchProps) {
+    const token = await fetchToken();
     const response = await axios.get("https://api.spotify.com/v1/search",
         {headers: { Authorization: 'Bearer ' + token}, params: {q: props.keyword, type: 'track'}},
     )
